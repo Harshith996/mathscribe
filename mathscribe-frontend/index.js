ed1bb9d7cb7986336aa8b2045adc2b328dda9e58
anandsomayajula <131316326+anandsomayajula@users.noreply.github.com>
2023-04-22 00:36:32 -0700

    Created Drag & Drop Webpage

---
+++ mathscribe-frontend/index.js
@@ -0,0 +1,82 @@
+// /*jslint es6:true*/
+
+const dragArea = document.querySelector('.drag-area');
+const dragText = document.querySelector('.header');
+
+let button = document.getElementById('bob');
+
+let input = document.querySelector('input');
+
+let file;
+
+
+// button.onclick = () =>{    
+//     input.click();
+// };
+
+$('.file-upload').on('click', function(e) {
+  e.preventDefault();
+  $('#file-input').trigger('click');
+});
+//when browse is clicked:
+
+input.addEventListener('change', function()  {
+    file = this.files[0];
+    dragArea.classList.add('active');
+    displayFile();
+
+})
+
+//when file is inside the drag area
+dragArea.addEventListener('dragover', (event)=>{
+    event.preventDefault();
+    dragText.textContent = 'Release to Upload';
+    dragArea.classList.add('active');
+    console.log('File is inside the drag area');
+});
+    
+//when file leaves drag area
+    
+dragArea.addEventListener('dragleave',()=>{
+    dragText.textContent = 'Drag & Drop';
+    dragArea.classList.remove('active');
+  //  console.log('File left the drag area');
+});
+
+//when the file is dropped
+dragArea.addEventListener('drop', (event) =>{
+    event.preventDefault();
+    file = event.dataTransfer.files[0];
+    //console.log(file);
+    displayFile();
+});
+
+
+function displayFile(){
+     let filetype = file.type;
+    let validExtensions= ['image/jpeg', 'image/jpg', 'image/png','application/pdf'];
+    
+    if(validExtensions.includes(filetype)){
+        let fileReader = new FileReader();
+        fileReader.onload = () =>{
+            let fileURL = fileReader.result;
+            let imgTag = `<img src = "${fileURL}" alt = "">`;
+            dragArea.innerHTML = imgTag;
+     };
+    
+     fileReader.readAsDataURL(file);
+    }
+    else{
+        alert('This file is not an Image!');
+        dragArea.classList.remove('active');
+        
+    }
+        
+    }
+  //  console.log('File is dropped');
+
+
+
+
+
+
